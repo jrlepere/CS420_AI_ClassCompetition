b93dcde6b6a40459e46d7ae1eb4e1e4fed424311
toddnguyen47 <todd.nguyen47@gmail.com>
2018-03-03 20:44:03 -0800

    Implemented a new random function for Zobrist

--- Connect4/algorithms/minimax/hash_tables/ZobristHash.cpp
+++ Connect4/algorithms/minimax/hash_tables/ZobristHash.cpp
@@ -1,36 +1,45 @@
-#include "ZobristHash.h"
+#include "ZobristHash.H"
 
 ZobristHash::ZobristHash() {
 	// Initialize the board once
-	srand(time(0)); //randomize seed
+	int numberOfPieces = 2; // 2 pieces, 1 and -1
+	unsigned seed1 = std::chrono::system_clock::now().time_since_epoch().count();
+	std::default_random_engine generator(seed1);
+	
+	std::uniform_int_distribution<int> distribution(0, LONG_MAX);
 	
 	for (int i = 0; i < BOARD_SIZE; i++) {	
-		vector<vector<int> > temp;
+		vector< vector <int> > temp;
 		for (int j = 0; j < BOARD_SIZE; j++) {
-			int v1 = rand();
-			int v2 = rand();
-			std::cout << v1 << "   " << v2 << "\n";
-			temp.push_back({v1, v2});
+			int rand1 = distribution(generator);
+			int rand2 = distribution(generator);
+			std::cout << "(" << rand1 << ", " << rand2 << ")\t";
+			temp.push_back({rand1, rand2});
 		}
-		initValues.push_back(temp);
-		temp.clear();
+		initValues.push_back(temp);	
+		vector< vector <int> > temp2;
+		temp.swap(temp2); // clear vector
 	}
-
 }
 
-int ZobristHash::getHashValue(vector<vector<int> >& board) {
+ZobristHash::~ZobristHash() {
+	vector< vector <vector<int> > > emptyVector;
+	initValues.swap(emptyVector);
+}
+
+int ZobristHash::getHashValue(vector< vector<int> > &board) {
 	int hashValue = 0;
 	
 	// For hash, 0 index = 1, 1st index = -1
-	for (int i = 0; i < BOARD_SIZE; i++) {
-		for (int j = 0; j < BOARD_SIZE; j++) {
+	for (int i = 0; i < board.size(); i++) {
+		for (int j = 0; j < board[i].size(); j++) {
 			// Hash for Player Max (1)
 			if (board[i][j] == MAX) {
-				hashValue ^= initValues[i][j][0];
+				hashValue = hashValue ^ initValues[i][j][0];
 			}
 			// Hash for Player Min
 			else if (board[i][j] == MIN) {
-				hashValue ^= initValues[i][j][1];
+				hashValue = hashValue ^ initValues[i][j][1];
 			}
 		}
 	}
--- Connect4/algorithms/minimax/hash_tables/ZobristHash.h
+++ Connect4/algorithms/minimax/hash_tables/ZobristHash.h
@@ -1,20 +1,24 @@
 #ifndef ZOBRISTHASH_H_
 #define ZOBRISTHASH_H_
 
+using namespace std;
+
 #include <string>
 #include <vector>
 #include <iostream>
-#include <cstdlib>
-#include <time.h>
+#include <iostream>
+#include <random>
+#include <climits>
+#include <chrono>
 #include "../../../src/Utilities.h"
 
 class ZobristHash {
 public:
 	ZobristHash();
-	~ZobristHash(){};
-	int getHashValue(vector<vector<int> >& board);
+	~ZobristHash();
+	int getHashValue(vector< vector<int> > &board);
 private:
-	vector<vector<vector<int> > > initValues;
+	vector< vector <vector<int> > > initValues;
 };
 
 #endif /* ZOBRISTHASH_H_ */
